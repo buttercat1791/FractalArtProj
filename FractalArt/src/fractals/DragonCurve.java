f48a7d73ffc4165fc6ee9f842955429b1d99a387
mjjurkoic <37635304+mjjurkoic@users.noreply.github.com>
2018-04-23 14:50:20 -0500

    Uses Graphics2D in paintComponent

--- FractalArt/src/fractals/DragonCurve.java
+++ FractalArt/src/fractals/DragonCurve.java
@@ -2,6 +2,7 @@ package fractals;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -15,9 +16,10 @@ public class DragonCurve extends JPanel {
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		Graphics2D G = (Graphics2D)g;
 		int width = getWidth();
 		int height = getHeight();
-		g.setColor(Color.WHITESMOKE);
-		g.drawLine(50, height / 2, width - 50, height / 2);
+		G.setColor(Color.WHITESMOKE);
+		G.drawLine(50, height / 2, width - 50, height / 2);
 	}
 }
