1251ff530e59faeda1f304f4a0487493239f8ee9
mjjurkoic <37635304+mjjurkoic@users.noreply.github.com>
2018-04-27 14:25:47 -0500

    DragonCurve now working

--- FractalArt/src/fractals/DragonCurve.java
+++ FractalArt/src/fractals/DragonCurve.java
@@ -3,28 +3,62 @@ package fractals;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JPanel;
 
+// Class borrows code from http://rosettacode.org/wiki/Dragon_curve#Java
 public class DragonCurve extends JPanel {
 	
-	String init = "FX";
-	String X = "X+YF+";
-	String Y = "-FX-Y";
-	
-	public DragonCurve() {
-		
-	}
-	
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D)g;
-		
-		int width = this.getWidth();
-		int height = this.getHeight();
-
-		g2.setPaint(Color.red);
-		g2.drawLine(50, height / 2, width - 50, height / 2);
-	}
+	private List<Integer> turns;
+    private double startingAngle, side;
+    private int iter;
+ 
+    public DragonCurve() {
+    	iter = 10;	// Magic number for testing, remove when slider is implemented
+        turns = getSequence(iter);
+        startingAngle = -iter * (Math.PI / 4);
+        side = 400 / Math.pow(2, iter / 2.);
+    }
+ 
+    public List<Integer> getSequence(int iterations) {
+        List<Integer> turnSequence = new ArrayList<Integer>();
+        for (int i = 0; i < iterations; i++) {
+            List<Integer> copy = new ArrayList<Integer>(turnSequence);
+            Collections.reverse(copy);
+            turnSequence.add(1);
+            for (Integer turn : copy) {
+                turnSequence.add(-turn);
+            }
+        }
+        return turnSequence;
+    }
+    
+    public void setIter(int newIter) {
+    	this.iter = newIter;
+    }
+ 
+    @Override
+    public void paint(Graphics g) {
+    	int width = this.getWidth();
+    	int height = this.getHeight();
+        g.setColor(Color.BLACK);
+        double angle = startingAngle;
+        int x1 = 230, y1 = 350;
+        int x2 = x1 + (int) (Math.cos(angle) * side);
+        int y2 = y1 + (int) (Math.sin(angle) * side);
+        g.drawLine(x1, y1, x2, y2);
+        x1 = x2;
+        y1 = y2;
+        for (Integer turn : turns) {
+            angle += turn * (Math.PI / 2);
+            x2 = x1 + (int) (Math.cos(angle) * side);
+            y2 = y1 + (int) (Math.sin(angle) * side);
+            g.drawLine(x1, y1, x2, y2);
+            x1 = x2;
+            y1 = y2;
+        }
+    }
 }
