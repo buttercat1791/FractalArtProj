a705329626496c3a9b39fd6d1bc0b66f42eca2bd
William Kostuch <wkostuch@gmail.com>
2018-04-30 16:33:55 -0500

    Update Julia
    Still not working, we may just end up copying some code from somewhere else but if we do that then Julia's all done.

--- FractalArt/src/fractals/JuliaSet.java
+++ FractalArt/src/fractals/JuliaSet.java
@@ -27,7 +27,7 @@ public class JuliaSet extends JPanel
     // Reruns constructor function code to refresh the image
     public void setIter(int newIter, int w, int h) 
     {
-    	iter = newIter;
+    	iter = newIter + 1;
     	this.width = w;
     	this.height = h;
     }
@@ -43,7 +43,7 @@ public class JuliaSet extends JPanel
     //
     public BufferedImage buildJulia(int type)
     {
-    	ComplexNumbers c = new ComplexNumbers(.2, .5);
+    	ComplexNumbers c = new ComplexNumbers(-0.7, 0.27015);
     	BufferedImage image = new BufferedImage(width, height, type);
     	
     	//Build image by looping through each pixel and masking it
@@ -51,14 +51,22 @@ public class JuliaSet extends JPanel
     	{
     		for(int y = 0; y < height; y++)
     		{
-    			ComplexNumbers v = new ComplexNumbers((x - WIDTH/2), (y-HEIGHT/2));
+    			ComplexNumbers v = new ComplexNumbers((x - WIDTH/20000000), (y-HEIGHT/20000000));
     			for(int i = 0; i < iter; i++){
     			    v = v.multiply(v).add(c);
     			}
-    			if(v.norm() > iter*100000)
-    			   image.setRGB(x, y, 0xFF000000);
-    			else
-    			   image.setRGB(x, y, 0xFFFF0000);
+    			double vN = v.norm();
+    			if(vN <= iter*100000) image.setRGB(x, y, 0xFF000000);
+    			else if(vN <= iter*200000 ) image.setRGB(x,  y,  0xFF00FF00);
+    			else if(vN <= iter*300000 ) image.setRGB(x,  y,  0x045A6F);
+    			else if(vN <= iter*400000 ) image.setRGB(x,  y,  0xFF35EA);
+    			else if(vN <= iter*500000 ) image.setRGB(x,  y,  0xFBFF01);
+    			else if(vN <= iter*600000 ) image.setRGB(x,  y,  0xFF355D);
+    			else if(vN <= iter*700000 ) image.setRGB(x,  y,  0x0317DB);
+    			else if(vN <= iter*800000 ) image.setRGB(x,  y,  0x6DC183);
+    			else if(vN <= iter*900000 ) image.setRGB(x,  y,  0x60586C);
+    			else if(vN <= iter*1000000 ) image.setRGB(x,  y,  0xFE0A63);
+    			else image.setRGB(x, y, 0xFFFFFFFF);
     		    }
     		}
     	
