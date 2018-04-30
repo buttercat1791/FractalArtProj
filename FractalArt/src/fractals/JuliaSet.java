36f6aa745800b4d61c83e1bd4e115aca8d6903a4
William Kostuch <wkostuch@gmail.com>
2018-04-30 17:19:10 -0500

    Update Julia
    Took some code from Rosetta Code, so now we have something to play with.

--- FractalArt/src/fractals/JuliaSet.java
+++ FractalArt/src/fractals/JuliaSet.java
@@ -2,7 +2,7 @@ package fractals;
 
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
-
+import java.awt.Color;
 import javax.swing.JPanel;
 
 
@@ -47,37 +47,60 @@ public class JuliaSet extends JPanel
     	BufferedImage image = new BufferedImage(width, height, type);
     	
     	//Build image by looping through each pixel and masking it
-    	for(int x = 0; x < width; x++)
-    	{
-    		for(int y = 0; y < height; y++)
-    		{
-    			ComplexNumbers v = new ComplexNumbers((x - WIDTH/20000000), (y-HEIGHT/20000000));
-    			for(int i = 0; i < iter; i++){
-    			    v = v.multiply(v).add(c);
-    			}
-    			double vN = v.norm();
-    			if(vN <= iter*100000) image.setRGB(x, y, 0xFF000000);
-    			else if(vN <= iter*200000 ) image.setRGB(x,  y,  0xFF00FF00);
-    			else if(vN <= iter*300000 ) image.setRGB(x,  y,  0x045A6F);
-    			else if(vN <= iter*400000 ) image.setRGB(x,  y,  0xFF35EA);
-    			else if(vN <= iter*500000 ) image.setRGB(x,  y,  0xFBFF01);
-    			else if(vN <= iter*600000 ) image.setRGB(x,  y,  0xFF355D);
-    			else if(vN <= iter*700000 ) image.setRGB(x,  y,  0x0317DB);
-    			else if(vN <= iter*800000 ) image.setRGB(x,  y,  0x6DC183);
-    			else if(vN <= iter*900000 ) image.setRGB(x,  y,  0x60586C);
-    			else if(vN <= iter*1000000 ) image.setRGB(x,  y,  0xFE0A63);
-    			else image.setRGB(x, y, 0xFFFFFFFF);
-    		    }
-    		}
-    	
+
+    	/*
+    	 * This code taken from https://rosettacode.org/wiki/Julia_set#Java 
+    	 * and changed to use complex numbers
+    	 */
+        for (int x = 0; x < width; x++) 
+        {
+            for (int y = 0; y < height; y++) 
+            {
+                ComplexNumbers v = new ComplexNumbers(
+                		(1.5 * (x - width / 2) / (0.5 * width)),
+                		((y - height / 2) / (0.5 * height)));
+                float i = iter*10;
+                while (v.norm() < 4 && i > 0) 
+                {
+                    v = new ComplexNumbers(
+                    		v.x * v.x - v.y * v.y + -0.7,
+                    		2.0 * v.x * v.y + 0.27015);
+                    
+                    i--;
+                }
+                int col = Color.HSBtoRGB((iter*10 / i) % 1, 1, i > 0 ? 1 : 0);
+                image.setRGB(x, y, col);
+        //End of taken code
+
+            }
+        }
     	return image;
     }
     
     
-    
-    
-    
-    
-    
-    
 }
+
+/*
+for(int x = 0; x < width; x++)
+{
+	for(int y = 0; y < height; y++)
+	{
+		ComplexNumbers v = new ComplexNumbers(x - width, y-height);
+		for(int i = 0; i < iter; i++){
+		    v = v.multiply(v).add(c);
+		}
+		double vN = v.norm();
+		if(vN <= iter*1000) image.setRGB(x, y, 0xFF00000);
+		else if(vN <= iter*2000 ) image.setRGB(x,  y,  0xFF00FF00);
+		else if(vN <= iter*3000 ) image.setRGB(x,  y,  0x045A6F);
+		else if(vN <= iter*4000 ) image.setRGB(x,  y,  0xFF35EA);
+		else if(vN <= iter*5000 ) image.setRGB(x,  y,  0xFBFF01);
+		else if(vN <= iter*6000 ) image.setRGB(x,  y,  0xFF355D);
+		else if(vN <= iter*7000 ) image.setRGB(x,  y,  0x0317DB);
+		else if(vN <= iter*8000 ) image.setRGB(x,  y,  0x6DC183);
+		else if(vN <= iter*9000 ) image.setRGB(x,  y,  0x60586C);
+		else if(vN <= iter*200000 ) image.setRGB(x,  y,  0xFE0AF3);
+		else image.setRGB(x, y, 0xFFFFFFFF);
+	    }
+	}
+*/
